56497a14413647d979be2ca37feb3164818359fd
Michubil <Michubil@outlook.com>
2026-09-27 00:52:01 +0800

    release: v1.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "top.michubil.musictag"
         minSdk = 35
         targetSdk = 37
-        versionCode = 29
-        versionName = "1.2.3"
+        versionCode = 30
+        versionName = "1.2.4"
 
         ndk {
             abiFilters += "arm64-v8a"
